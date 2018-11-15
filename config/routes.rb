b2bb1aa49b7089473a93759ecc36bf49054e27b2
Russel <ruscruzel@gmail.com>
2018-11-15 00:21:48 -0600

    creación de controllers y views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'estaticas/contacto'
+  get 'estaticas/nosotros'
+
+  get 'home/index'
+  root 'home#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
